525c827f9fb504f3cbe274fa0eabbe4a33a5369c
Douglas Oliveira <douglaspo_97@outlook.com>
2022-02-22 12:07:44 -0300

    feat: real-time with graphql-subscriptions

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -9,8 +9,21 @@ export default {
     user: async (_, { id }) => await User.findById(id),
   },
   Mutation: {
-    createUser: (_, { data }) => User.create(data),
+    createUser: async (_, { data }, { pubsub }) => {
+      const user = await User.create(data);
+
+      pubsub.publish('USER_ADDED', {
+        userAdded: user
+      });
+
+      return user;
+    },
     updateUser: async (_, { id, data }) => await User.findOneAndUpdate(id, data, { new: true }),
     deleteUser: async (_, { id }) => !!(await User.findOneAndDelete(id)),
   },
+  Subscription: {
+    userAdded: {
+      subscribe: async (obj, args, { pubsub }) => await pubsub.asyncIterator('USER_ADDED'),
+    }
+  }
 };
--- src/startServer.js
+++ src/startServer.js
@@ -1,7 +1,65 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { PubSub } from 'graphql-subscriptions';
+import express from 'express';
+import http from 'http';
+
+import { execute, subscribe } from 'graphql';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+
 import mongoose from 'mongoose';
 
-function startServer({ typeDefs, resolvers }) {
+async function startApolloServer({ typeDefs, resolvers }) {
+  // Required logic for integrating with Express
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const pubsub = new PubSub();
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers, contet: { pubsub } });
+
+  const subscriptionServer = SubscriptionServer.create({
+    // This is the `schema` we just created.
+    schema,
+    // These are imported from `graphql`.
+    execute,
+    subscribe,
+ }, {
+    // This is the `httpServer` we created in a previous step.
+    server: httpServer,
+    // Pass a different path here if your ApolloServer serves at
+    // a different path.
+    path: '/graphql',
+ });
+
+  // Same ApolloServer initialization as before, plus the drain plugin.
+  const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            subscriptionServer.close();
+          }
+        };
+      }
+    }],
+  });
+  
+
+  // More required logic for integrating with Express
+  await server.start();
+  server.applyMiddleware({
+    app,
+
+    // By default, apollo-server hosts its GraphQL endpoint at the
+    // server root. However, *other* Apollo Server packages host it at
+    // /graphql. Optionally provide this to match apollo-server.
+    path: '/'
+  });
+
+
   mongoose.connect('mongodb://localhost:27017/graphql', {
     authSource: "admin",
     user: "root",
@@ -10,8 +68,9 @@ function startServer({ typeDefs, resolvers }) {
     useUnifiedTopology: true,
   })
 
-  const server = new ApolloServer({ typeDefs, resolvers });
-  server.listen().then(({ url }) => console.log(`🔥 Server started at ${url}`));
-}
+  // Modified server startup
+  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+};
 
-export default startServer;
+export default startApolloServer;
